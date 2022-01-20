f0de8df6d178ce8aa598351967542bb2622a26c5
Bhaskar Das <bhaskardas090@gmail.com>
2022-01-20 12:00:11 +0530

    New Product Page Completed

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -6,12 +6,18 @@ export default function NewProduct() {
       <h1>New Product</h1>
       <form className={styles.form}>
         <label htmlFor="imageFile">Image</label>
-        <input type="file" name="imgFile" id="imgFile" />
+        <input type="file" name="imgFile" id="imgFile" className={styles.file}/>
         <label htmlFor="productname">Name</label>
-        <input type="text" name="productname" id="productname" />
+        <input type="text" placeholder="apple airpod" name="productname" id="productname" className={styles.input}/>
         <label htmlFor="stock">Stock</label>
-        <input type="text" name="stock" id="stock" />
+        <input type="text" placeholder="123" name="stock" id="stock" className={styles.input}/>
+        <label htmlFor="active">Active</label>
+        <select name="active" id="active" className={styles.input}>
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+        </select>
       </form>
+      <button className={styles.createButton}>Create</button>
     </div>
   )
 }
