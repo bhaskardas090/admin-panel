0f4876bbe396bd286360aab9915781b988c8d989
Bhaskar Das <bhaskardas090@gmail.com>
2022-01-21 21:26:36 +0530

    RESPONSIVE: product section mobile responsiveness added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,13 +3,16 @@ import notification from '../../img/notification.png'
 import setting from '../../img/setting.png'
 import language from '../../img/world.png'
 import avatar from '../../img/man.png'
-
+import { Link } from 'react-router-dom';
 
 export default function Topbar() {
   return (
     <div className={styles.topbar}>
+      
       <div className={styles.topbarLogo}>
-        iambhaskar
+        <Link to="/" className={styles.link}>
+          iambhaskar
+        </Link>
       </div>
       <div className={styles.topbarIcons}>
         <div className={styles.topbarIcon}>
